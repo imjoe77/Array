46329cab67e2d330d8e72c4cd6fb9b7d5dcf3bb9
Joe <nathanielbandi636@gmail.com>
2025-11-21 15:56:13 +0530

    Update array.py

--- array.py
+++ array.py
@@ -4,8 +4,10 @@ if len(sys.argv) == 21:
     script_name = sys.argv[0]
     array = [None] * 11
     array[10] = int(sys.argv[1])   
+    print("User provided inputs")
 else:
-    array = [1, 2, 3, 9, 7, 10, 5]   
+    array = [1, 2, 3, 9, 7, 10, 5]  
+    print("Default input")
 
 sum_of_elements = sum(array)
 print("Sum of array elements:", sum_of_elements)
