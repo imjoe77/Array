f023d2baed80e66268b24bdbd4de5510502aa437
Joe <nathanielbandi636@gmail.com>
2025-11-21 21:53:26 +0530

    Refactor array input and calculation logic

--- array.py
+++ array.py
@@ -1,21 +1,14 @@
-# array_input.py
-
-# Ask user for number of inputs
-n = int(input("How many numbers do you want to enter? "))
+# array_input_5.py
 
 array = []
 
-for i in range(n):
+for i in range(5):
     value = int(input(f"Enter number {i+1}: "))
     array.append(value)
 
 print("\nYou entered:", array)
 
-sum_of_elements = sum(array)
-print("Sum:", sum_of_elements)
-
-avg_of_elements = sum_of_elements / len(array)
-print("Avg:", avg_of_elements)
-
+print("Sum:", sum(array))
+print("Avg:", sum(array) / len(array))
 print("Max:", max(array))
 print("Min:", min(array))
