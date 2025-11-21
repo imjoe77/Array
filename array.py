4f9176e16924fcf07f421ab05984369be7ddc478
Joe <nathanielbandi636@gmail.com>
2025-11-21 22:50:46 +0530

    Update array.py

--- array.py
+++ array.py
@@ -1,36 +1,18 @@
-# array_flexible.py
-import os, sys
+import sys
 
-def parse_list_from_string(s):
-    return [int(x) for x in s.strip().split() if x.strip()]
-
-array = None
-
-# 1) CLI args (preferred)
-if len(sys.argv) > 1:
-    try:
-        array = [int(x) for x in sys.argv[1:]]
-        print("User input (CLI args)")
-    except ValueError:
-        print("Invalid CLI args. Falling back to other sources.")
-
-# 2) Environment var or Jenkins single param ALL_NUMS or ARRAY_INPUT
-if array is None:
-    env = os.environ.get('ALL_NUMS') or os.environ.get('ARRAY_INPUT')
-    if env:
-        try:
-            array = parse_list_from_string(env)
-            print("User input (env ALL_NUMS/ARRAY_INPUT)")
-        except ValueError:
-            array = None
-
-# 3) default
-if not array:
+# Expect exactly 10 user inputs
+if len(sys.argv) == 11:
+    array = [int(x) for x in sys.argv[1:]]
+    print("User input")
+else:
     array = [1, 2, 3, 9, 7, 10, 5]
-    print("Default input")
+    print("Default Input")
 
-print("Input array:", array)
-print("Sum:", sum(array))
-print("Avg:", sum(array)/len(array))
-print("Max:", max(array))
-print("Min:", min(array))
+sum_of_elements = sum(array)
+print("Sum of array elements:", sum_of_elements)
+
+avg_of_elements = sum_of_elements / len(array)
+print("Average of array elements:", avg_of_elements)
+
+print("Max element in array:", max(array))
+print("Min element in array:", min(array))
