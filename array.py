50c3c65a5646b4fe3e44b6078bd2dd857a548ab9
Joe <nathanielbandi636@gmail.com>
2025-11-21 16:27:52 +0530

    Update array.py

--- array.py
+++ array.py
@@ -1,19 +1,17 @@
 import sys
 
-if len(sys.argv) == 21:
-    script_name = sys.argv[0]
-    array = [None] * 11
-    array[10] = int(sys.argv[1])   
+if len(sys.argv) == 11:
     print("User provided inputs")
+    array = [int(sys.argv[i]) for i in range(1, 11)]
 else:
-    array = [1, 2, 3, 9, 7, 10, 5]  
     print("Default input")
+    array = [1, 2, 3, 9, 7, 10, 5]
 
 sum_of_elements = sum(array)
-print("Sum of array elements:", sum_of_elements)
+print("Sum:", sum_of_elements)
 
 avg_of_elements = sum_of_elements / len(array)
-print("Average of array elements:", avg_of_elements)
+print("Avg:", avg_of_elements)
 
-print("Max element in array:", max(array))
-print("Min element in array:", min(array))
+print("Max:", max(array))
+print("Min:", min(array))
